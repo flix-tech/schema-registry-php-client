1ebddd96815f2b5d4ea0ac8ba2c1b568cf365109
Thomas Ploch <thomas.ploch@flixbus.com>
2020-10-02 23:29:40 +0200

    [UR] Created new Json.php function container

---
+++ src/Json.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FlixTech\SchemaRegistryApi;
+
+final class Json
+{
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+}
