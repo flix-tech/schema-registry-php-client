ce3f06e1548ca72998b946be1e31da8cbb41252c
Thomas Ploch <profiploch@gmail.com>
2020-12-11 07:25:45 +0100

    Internal fullName variable in `AvroReference`

--- src/Schema/AvroReference.php
+++ src/Schema/AvroReference.php
@@ -6,4 +6,13 @@ namespace FlixTech\SchemaRegistryApi\Schema;
 
 final class AvroReference
 {
+    /**
+     * @var string
+     */
+    private $fullName;
+
+    public function __construct(string $fullName)
+    {
+        $this->fullName = $fullName;
+    }
 }
