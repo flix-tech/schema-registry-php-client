02d3776f0a9150a427ead94a621740197b94ddd4
Thomas Ploch <profiploch@gmail.com>
2020-12-11 07:23:42 +0100

    Finalize construction test for AvroReference.php

--- test/Schema/AvroReferenceTest.php
+++ test/Schema/AvroReferenceTest.php
@@ -2,7 +2,9 @@
 
 namespace FlixTech\SchemaRegistryApi\Test\Schema;
 
+use FlixTech\SchemaRegistryApi\Schema\AvroReference;
 use Generator;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class AvroReferenceTest extends TestCase
@@ -11,12 +13,18 @@ class AvroReferenceTest extends TestCase
      * @dataProvider avroReferences
      * @test
      */
-    public function it_should_only_be_constructable_from_a_valid_Avro_reference(string $fullName, bool $isValid): void {}
+    public function it_should_only_be_constructable_from_a_valid_Avro_reference(string $fullName, bool $isValid): void {
+        if (!$isValid) {
+            $this->expectException(InvalidArgumentException::class);
+        }
+
+        $this->assertSame((string) new AvroReference($fullName), $fullName);
+    }
 
     public static function avroReferences(): Generator {
         yield 'Valid root name' => ['test', true];
-//         yield 'Valid full name' => ['test.example', true];
-//         yield 'Empty full name' => ['', false];
-//        yield 'Invalid full name' => ['-test', false];
+        yield 'Valid full name' => ['test.example', true];
+        yield 'Empty full name' => ['', false];
+        yield 'Invalid full name' => ['-test', false];
     }
 }
