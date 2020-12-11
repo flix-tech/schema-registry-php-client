67200e5ee148883f77fed98aeabca1781369826f
Thomas Ploch <profiploch@gmail.com>
2020-12-11 07:03:41 +0100

    [TEST] Added empty `AvroReferenceTest`

---
+++ test/Schema/AvroReferenceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FlixTech\SchemaRegistryApi\Test\Schema;
+
+use FlixTech\SchemaRegistryApi\Schema\AvroReference;
+use PHPUnit\Framework\TestCase;
+
+class AvroReferenceTest extends TestCase
+{
+
+}
