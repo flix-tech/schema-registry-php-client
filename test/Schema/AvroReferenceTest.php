2a73d050eb0a7cd1f3d6b435b1f2448b8ae9079d
Thomas Ploch <profiploch@gmail.com>
2020-12-11 07:13:48 +0100

    Add avroReferences data provider annotation to test

--- test/Schema/AvroReferenceTest.php
+++ test/Schema/AvroReferenceTest.php
@@ -7,6 +7,9 @@ use PHPUnit\Framework\TestCase;
 
 class AvroReferenceTest extends TestCase
 {
+    /**
+     * @dataProvider avroReferences
+     */
     public function it_should_only_be_constructable_from_a_valid_Avro_reference(): void {}
 
     public static function avroReferences(): Generator {}
