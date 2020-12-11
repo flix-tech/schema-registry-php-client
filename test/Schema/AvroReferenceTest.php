78c49f9af3a6079765609fb742e760ca0e2cd2f7
Thomas Ploch <profiploch@gmail.com>
2020-12-11 07:13:16 +0100

    Add avroReferences data provider

--- test/Schema/AvroReferenceTest.php
+++ test/Schema/AvroReferenceTest.php
@@ -2,9 +2,12 @@
 
 namespace FlixTech\SchemaRegistryApi\Test\Schema;
 
+use Generator;
 use PHPUnit\Framework\TestCase;
 
 class AvroReferenceTest extends TestCase
 {
     public function it_should_only_be_constructable_from_a_valid_Avro_reference(): void {}
+
+    public static function avroReferences(): Generator {}
 }
