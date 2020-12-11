4f7339e5a158c1d517231b98b7ef8dadb84b7270
Thomas Ploch <profiploch@gmail.com>
2020-12-11 09:43:19 +0100

    Make AvroReference final

--- src/Schema/AvroReference.php
+++ src/Schema/AvroReference.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace FlixTech\SchemaRegistryApi\Schema;
 
-class AvroReference implements \JsonSerializable
+final class AvroReference implements \JsonSerializable
 {
     /**
      * @var AvroName
