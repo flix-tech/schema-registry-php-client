f68ce91d84949ed31dab864c58af489f02d25780
Thomas Ploch <profiploch@gmail.com>
2020-12-11 07:18:41 +0100

    Added test scenarios to dataProvider

--- test/Schema/AvroReferenceTest.php
+++ test/Schema/AvroReferenceTest.php
@@ -12,5 +12,10 @@ class AvroReferenceTest extends TestCase
      */
     public function it_should_only_be_constructable_from_a_valid_Avro_reference(): void {}
 
-    public static function avroReferences(): Generator {}
+    public static function avroReferences(): Generator {
+        yield 'Valid root name' => ['test', true];
+//         yield 'Valid full name' => ['test.example', true];
+//         yield 'Empty full name' => ['', false];
+//        yield 'Invalid full name' => ['-test', false];
+    }
 }
